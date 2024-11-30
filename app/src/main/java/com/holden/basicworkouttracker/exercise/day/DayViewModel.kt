bd032fa162462253d801029bc61879e77a89ed56
Malachi <malachi.coqui@gmail.com>
2024-11-30 17:55:53 -0500

    Use datepicker with utc

--- app/src/main/java/com/holden/basicworkouttracker/exercise/day/DayViewModel.kt
+++ app/src/main/java/com/holden/basicworkouttracker/exercise/day/DayViewModel.kt
@@ -5,14 +5,20 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.platform.LocalContext
 import com.holden.basicworkouttracker.LOCAL_PLATES
 import com.holden.basicworkouttracker.exercise.Exercise
+import com.holden.basicworkouttracker.exercise.ExerciseForDay
 import com.holden.basicworkouttracker.exercise.Workout
 import com.holden.basicworkouttracker.loadPlates
+import com.holden.basicworkouttracker.util.NullableScope
 import com.holden.basicworkouttracker.util.OrderedMap
 import com.holden.basicworkouttracker.util.bindNullable
 import com.holden.basicworkouttracker.util.removed
 import com.holden.basicworkouttracker.util.replaced
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.datetime.Instant
+import kotlinx.datetime.LocalDate
+import kotlinx.datetime.TimeZone
+import kotlinx.datetime.toLocalDateTime
 
 class DayViewModel(
     val exerciseKey: MutableStateFlow<String?>,
@@ -60,6 +66,12 @@ class DayViewModel(
     @Composable
     get() = weightForCalculator != null
 
+    val _showCalendar = MutableStateFlow(false)
+
+    val showCalendar: Boolean
+        @Composable
+        get() = _showCalendar.collectAsState().value
+
     fun addSet(
         set: Workout
     ) = bindNullable {
@@ -97,37 +109,54 @@ class DayViewModel(
         _weightForCalculator.value = null
     }
 
+    fun showCalendar() {
+        _showCalendar.value = true
+    }
+
+    fun hideCalendar() {
+        _showCalendar.value = false
+    }
+
+    private val NullableScope.key
+        get() = exerciseKey.value.bind()
+    private val NullableScope.exercise
+        get() = exercises.value[key].bind()
+    private val NullableScope.exerciseForDay
+        get() = exercise.history[dayIndex.bind()]
+
+    fun onDateUpdated(newDate: Instant) = bindNullable {
+        updateExerciseForDay(
+            exerciseForDay.copy(date = newDate.toLocalDateTime(TimeZone.UTC).date)
+        )
+        hideCalendar()
+    }
 
     fun removeSet(
         setIndex: Int
     ) = bindNullable {
-        val exercise = exercises.value[exerciseKey.value].bind()
-        val exerciseForDay = exercise.history[dayIndex.bind()]
+        updateExerciseForDay(
+            exerciseForDay.copy(
+                sets = exerciseForDay.sets.removed(setIndex)
+            )
+        )
+    }
+
+    fun updateSet(newSet: Workout) = bindNullable {
+        val setIndex = _editSetIndex.value.bind()
+        updateExerciseForDay(exerciseForDay.copy(
+            sets = exerciseForDay.sets.replaced(setIndex, newSet)
+        ))
+        _editSetIndex.value = null
+    }
+
+    private fun updateExerciseForDay(newDay: ExerciseForDay) = bindNullable {
         exercises.value.replace(
             exercise.copy(
-                history = exercise.history.replaced(dayIndex.bind(), exerciseForDay.copy(
-                    sets = exerciseForDay.sets.removed(setIndex)
-                ))
+                history = exercise.history.replaced(dayIndex.bind(), newDay)
             ),
             exerciseKey.value.bind()
         )
     }?.let(setExercises)
-
-    fun updateSet(newSet: Workout) = bindNullable {
-        val setIndex = _editSetIndex.value.bind()
-        val exercise = exercises.value[exerciseKey.value].bind()
-        val exerciseForDay = exercise.history[dayIndex.bind()]
-        exercises.value.replace(
-            exercise.copy(
-                history = exercise.history.replaced(dayIndex.bind(), exerciseForDay.copy(
-                    sets = exerciseForDay.sets.replaced(setIndex, newSet)
-                ))
-            ),
-            exerciseKey.value.bind()
-        )
-    }?.let(setExercises).also {
-        _editSetIndex.value = null
-    }
 
     @Composable
     fun loadPersistedPlates() = LocalContext.current.loadPlates(LOCAL_PLATES)
