83efa1c1ca2426ad78d82449424b5dd9550de3a4
Malachi <malachi.coqui@gmail.com>
2024-11-29 19:35:48 -0500

    update version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.holden.basicworkouttracker"
         minSdk = 24
         targetSdk = 34
-        versionCode = 1
-        versionName = "0.0"
+        versionCode = 2
+        versionName = "0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
