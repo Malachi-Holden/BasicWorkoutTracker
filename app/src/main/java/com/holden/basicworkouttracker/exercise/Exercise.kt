94be95488937a87902e6b9b929958ef6f53a64f3
Malachi <malachi.coqui@gmail.com>
2024-12-07 08:34:28 -0500

    add default values to notes so that it can load from old data

--- app/src/main/java/com/holden/basicworkouttracker/exercise/Exercise.kt
+++ app/src/main/java/com/holden/basicworkouttracker/exercise/Exercise.kt
@@ -9,28 +9,28 @@ import kotlinx.serialization.Serializable
 @Serializable
 data class ExerciseGroup(
     val title: String,
-    val notes: String,
+    val notes: String = "",
     val exerciseIds: List<String>
 )
 
 @Serializable
 data class Exercise(
     val title: String,
-    val notes: String,
+    val notes: String = "",
     val history: List<ExerciseForDay>,
     val showOnHomepage: Boolean = true
 )
 
 @Serializable
 data class ExerciseForDay(
-    val notes: String,
+    val notes: String = "",
     val date: LocalDate?,
     val sets: List<Workout>
 )
 
 @Serializable
 data class Workout(
-    val notes: String,
+    val notes: String = "",
     val reps: Int,
     val weight: Double
 )
