199d8b23bbd88b3e6f2ca23376584ba2a89f131f
Malachi <malachi.coqui@gmail.com>
2025-01-28 20:38:47 -0500

    clean up

--- app/src/main/java/com/holden/basicworkouttracker/home/HomePage.kt
+++ app/src/main/java/com/holden/basicworkouttracker/home/HomePage.kt
@@ -3,11 +3,9 @@ package com.holden.basicworkouttracker.home
 import android.widget.Toast
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -20,7 +18,6 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.PlatformTextStyle
